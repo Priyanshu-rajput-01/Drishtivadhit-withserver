4cb2d12d6bae977cb9326eb6f278fc8a2a8bf28d
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-10-25 20:15:40 +0530

    Update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,11 +6,8 @@ const Grid = require('gridfs-stream');
 
 const db =   mongoose.connection;
 db.on('error',console.error.bind("Error while connection to the server"));
-
 db.once('open',async function(){
     console.log('DataBase is up and running !!');
 })
 
-
-// module.exports = {db:db,gfs:gfs};
 module.exports = db;
